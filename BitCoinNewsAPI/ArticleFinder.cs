0daca9a05c73a16f4360de8eb543c1307a9c6aa0
Christopher Drymon <chrisdrymon@yahoo.com>
2020-04-30 17:22:02 -0500

    Add project files.

---
+++ BitCoinNewsAPI/ArticleFinder.cs
@@ -0,0 +1,62 @@
+﻿using NewsAPI;
+using NewsAPI.Models;
+using NewsAPI.Constants;
+using System;
+using System.Threading;
+
+namespace BitCoinNewsAPI
+{
+    class ArticleFinder
+    {
+        static void Main(string[] args)
+        {
+            // init with your API key
+            var newsApiClient = new NewsApiClient("81c072dd1ec540069da4d4069ce99f39");
+            int hitsperpage = 80;
+            int i = 2;
+            int j = 1;
+            int tot_arts = 300;
+            while (i <= tot_arts/hitsperpage)
+            {
+                var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+                {
+                    Q = "Bitcoin",
+                    SortBy = SortBys.Popularity,
+                    Language = Languages.EN,
+                    From = new DateTime(2020, 4, 1),
+                    PageSize = hitsperpage,
+                    Page = i
+                });
+
+                if (articlesResponse.Status == Statuses.Ok)
+                {
+                    // total results found
+                    tot_arts = articlesResponse.TotalResults;
+                    int start = (i - 1) * 100 + 1;
+                    int end = start + 99;
+                    Console.WriteLine(String.Format("Articles {0}-{1} of {2}", start, end, tot_arts));
+                    foreach (var article in articlesResponse.Articles)
+                    {
+                        // title
+                        Console.WriteLine(j.ToString(), article.Title);
+                        // url
+                        Console.WriteLine(article.Url);
+                        // published at
+                        Console.WriteLine(article.PublishedAt);
+
+                        if (article.Author != null && article.Description != null && article.PublishedAt != null && article.Title != null)
+                        {
+                            // Convert to unixtime
+                            long unixtime = ((DateTimeOffset)article.PublishedAt).ToUnixTimeSeconds();
+                            DataAccess.InsertNews((DateTime)article.PublishedAt, unixtime, article.Url, article.Title, article.Author, article.Description);
+                            j++;
+                        }
+                    }
+                }
+                Thread.Sleep(3000);
+                i++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BitCoinNewsAPI/DataAccess.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BitCoinNewsAPI
+{
+    class DataAccess
+    {
+        private static string constring = ConfigurationManager.ConnectionStrings["BCArticles"].ConnectionString;
+        public static void InsertNews(DateTime datetime, long unixtime, string url, string title, string author, string description)
+        {
+            using (SqlConnection conn = new SqlConnection(constring))
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertNews", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@datetime", datetime);
+                    cmd.Parameters.AddWithValue("@unixtime", unixtime);
+                    cmd.Parameters.AddWithValue("@url", url);
+                    cmd.Parameters.AddWithValue("@title", title);
+                    cmd.Parameters.AddWithValue("@author", author);
+                    cmd.Parameters.AddWithValue("@description", description);
+                    conn.Open();
+                    int result = cmd.ExecuteNonQuery();
+                    if (result < 0)
+                        Console.WriteLine("Error inserting data into Database!");
+                }
+            }
+        }
+    }
+}
